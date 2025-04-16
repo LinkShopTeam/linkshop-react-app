a980f6e32b660ff044d5bfca9fbb637d53132369
Jin <spiffreact@gmail.com>
2025-04-16 15:47:11 +0900

    feat: 불필요파일삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppMain from './AppMain';
-import LinkPostPage from './pages/LinkPostPage/LinkPost';
-import ListPage from './pages/ListPage/LinkPage';
+import LinkPostPage from './pages/LinkPostPage/linkpost';
+import ListPage from './pages/ListPage/listpage';
 
 export default function App() {
   return (
