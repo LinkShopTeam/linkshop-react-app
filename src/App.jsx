b969ebb3342c56087d43409d26096ec483e992aa
shm41503 <114830491+shm41503@users.noreply.github.com>
2025-04-18 14:20:41 +0900

    Update App.jsx
    LinkDetailPage.jsx 이름 고침

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppMain from './AppMain';
-import LinkPostPage from './pages/LinkPostPage/linkpost';
+import LinkPostPage from "./pages/LinkPostPage/LinkPostPage";
 import ListPage from './pages/ListPage/listpage';
-import LinkDetailPage from './pages/LinkDetailPage';
+import LinkDetailPage from './pages/LinkDetailPage/LinkDetailPage';
 
 export default function App() {
   return (
