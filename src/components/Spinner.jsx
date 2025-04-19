4d5907f2abd5e346209c748a5cc5408177d697ab
Jin <spiffreact@gmail.com>
2025-04-19 19:02:24 +0900

    feat:ShopCard 불필요하여제거

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,10 @@
-import { RotatingLines } from 'react-loader-spinner';
-import styles from '/src/styles/components/Spinner.module.css';
+// import { RotatingLines } from 'react-loader-spinner';
+// import styles from '/src/styles/components/Spinner.module.css';
 
-export const Spinner = () => {
-  return (
-    <div className={styles.spinner}>
-      <RotatingLines width='100' strokeColor='#4A4F5A' />
-    </div>
-  );
-};
+// export const Spinner = () => {
+//   return (
+//     <div className={styles.spinner}>
+//       <RotatingLines width='100' strokeColor='#4A4F5A' />
+//     </div>
+//   );
+// };
