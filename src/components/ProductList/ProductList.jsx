18ac17dedda21384cf12c095a9f78a8ad3ff4141
geesunny <jiseonyang08@gmail.com>
2025-04-24 14:10:35 +0900

    chore: LinkCard/ProductList 폴더 구조 변경 및 import 경로 수정

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,7 +2,7 @@
 // 상품 이미지를 보여주는 컴포넌트입니다.
 // 상품 이미지를 최대 3개까지만 보여줍니다.
 
-import styles from '../styles/components/ProductList.module.css';
+import styles from './ProductList.module.css';
 
 const ProductList = ({ products }) => {
   const limited = products.slice(0, 3); // 최대 3개까지만 보여줌
