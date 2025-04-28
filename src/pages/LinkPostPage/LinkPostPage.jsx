cf50baa58dc78fc1b8b158b8e315df6e2f5967d1
shm41503 <114830491+shm41503@users.noreply.github.com>
2025-04-28 14:52:00 +0900

    feat: toast and successmodal 경로 다시 정리

--- src/pages/LinkPostPage/LinkPostPage.jsx
+++ src/pages/LinkPostPage/LinkPostPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './LinkPostPage.module.css';
-import Toast from '../../components/Toast';
+import SuccessModal from '../../components/SuccessModal/SuccessModal';
+import Toast from '../../components/Toast/Toast';
 import SuccessModal from '../../components/SuccessModal';
 
 const LinkPostPage = () => {
